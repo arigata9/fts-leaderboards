1802fbe66803d941b9054eccd3e638b34a1532b2
arigata9 <56773132+arigata9@users.noreply.github.com>
2022-01-17 22:00:57 +0100

    modyfing main file and routes

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const routes = require('./routes.js');
-
+const config = require('./config.json');
 const app = express();
+
+// routes file
+const routes = require('./routes/routes.js');
+
+// middlewares
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
 app.use('/', routes);
 
-app.get('/', (req, res) => {
-    res.send('Hello :)');
-});
+// starting server
+app.listen(config.port, config.address, () => {
+    console.log(`Listening on ${config.address} port ${config.port}`);
+})
