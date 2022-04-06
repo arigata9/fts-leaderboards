f517e865c7be8d326ecb8fd91144ca112bf1fa91
arigata9 <markus@arigata9.de>
2022-04-06 22:41:03 +0200

    Various fixes and adding preloader to start page

--- static/js/preloader.js
+++ static/js/preloader.js
@@ -1,22 +1,6 @@
 /* Preloader
- * -------------------------------------------------- */
-var clPreloader = () => {
+         * -------------------------------------------------- */
 
-    $("html").addClass('cl-preload');
-
-    $WIN.on('load', function () {
-        //force page scroll position to top at page refresh
-        $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-        // will first fade out the loading animation 
-        $("#loader").fadeOut("slow", function () {
-            // will fade out the whole DIV that covers the website.
-            $("#preloader").delay(300).fadeOut("slow");
-        });
-
-        // for hero content animations 
-        $("html").removeClass('cl-preload');
-        $("html").addClass('cl-loaded');
-
-    });
-};
+$(window).on("load", function () {
+    $(".loader-wrapper").fadeOut("slow");
+});
