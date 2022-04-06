779a1a845a2a62bdd72591869d02899fa89aa7bc
arigata9 <markus@arigata9.de>
2022-04-06 23:16:43 +0200

    wrong folder

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const locales = require('./locales.json');
+const locales = require('./../locales.json');
 
 // root
 router.get('/', (req, res) => {
