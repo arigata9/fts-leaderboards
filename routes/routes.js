81a4b69dce1ef438dfe430ed9ea48ebb9df33ba4
arigata9 <markus@arigata9.de>
2022-04-20 16:24:07 +0200

    Trying to implement mysql into view rendering

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,40 @@
 const express = require('express');
+const mysql = require('mysql');
 const router = express.Router();
 const locales = require('./../locales.json');
+
+let con = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPW,
+    database: process.env.MYSQLDB
+});
+
+con.connect((err) => {
+    if(err) throw err;
+    console.log('Database connection established.');
+});
+
+const mapsSQL = 'SELECT track_name FROM tracks;';
+const maps = con.query(mapsSQL, (err, result, fields) => {
+    if(err) throw err;
+    if(result.length == 0) return 'No maps found';
+
+    return result;
+});
 
 // root
 router.get('/', (req, res) => {
     res.render('index', {
-        en: locales.en
+        en: locales.en,
+        tracks: maps
     });
 });
 
 router.get('/ru', (req, res) => {
     res.render('index', {
-        ru: locales.ru
+        ru: locales.ru,
+        tracks: maps
     });
 });
 
